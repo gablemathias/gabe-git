c919e8437c822d1584e1ee6e66708b7e38562418
Gabriel Mathias <gablemathias@icloud.com>
2022-12-25 20:38:51 -0300

    create SHA1

--- main.rb
+++ main.rb
@@ -1,15 +1,16 @@
 require 'zlib'
+require 'digest'
 
 # 83baae61804e65cc73a7201a7252750c76066a30
 
 command = ARGV[0]
 
 case command
-when 'init'
-  Dir.mkdir('./.git')
-  Dir.mkdir('./.git/objects')
-  Dir.mkdir('./.git/refs')
-  File.write("./.git/HEAD", "ref: refs/heads/master\n")
+when "init"
+  Dir.mkdir("./.gabegit")
+  Dir.mkdir("./.gabegit/objects")
+  Dir.mkdir("./.gabegit/refs")
+  File.write("./.gabegit/HEAD", "ref: refs/heads/master\n")
   puts "Initialized myOwnGit directory"
 when "cat-file"
   flag = ARGV[1][1]
@@ -20,6 +21,13 @@ when "cat-file"
   raw_file = Zlib.inflate(file) if flag == 'p' # file with header: blob 10\x00
   file_source = raw_file[raw_file.index("\x00") + 1..]
   puts file_source
+when "hash-object"
+  file = ARGV[2]
+  flag = ARGV[1]
+  # transform the file input into a SHA1 HASH
+  sha1 = Digest::SHA1.hexdigest file
+  # Create folder with the first 2 chars of the SHA1 HASH
+  # Create a file with the rest of it inside the new folder
 else
   raise "Unknown command #{command}" # Runtime Error
 end
