86c8b0e6c320a6208a50134fe49f8d3bf5e4bed8
Gabriel Mathias <gablemathias@icloud.com>
2022-12-31 08:21:46 -0300

    add open and read obj methods to DRY

--- main.rb
+++ main.rb
@@ -3,6 +3,14 @@ require 'digest'
 
 # 83baae61804e65cc73a7201a7252750c76066a30
 
+def open_object(sha1)
+  File.read("./.git/objects/#{sha1[...2]}/#{sha1[2..]}")
+end
+
+def read_object(arg)
+  Zlib.inflate open_object(arg)
+end
+
 command = ARGV[0]
 
 case command
@@ -13,12 +21,7 @@ when "init"
   File.write("./.gabegit/HEAD", "ref: refs/heads/master\n")
   puts "Initialized myOwnGit directory"
 when "cat-file"
-  flag = ARGV[1][1]
-  folder = ARGV[2][...2]
-  file_name = ARGV[2][2..]
-  file_path = File.open("./.git/objects/#{folder}/#{file_name}")
-  file = file_path.read
-  raw_file = Zlib.inflate(file) if flag == 'p' # file with header: blob 10\x00
+  raw_file = read_object(ARGV[-1]) # file with header: blob 10\x00
   file_source = raw_file[raw_file.index("\x00") + 1..]
   puts file_source
 when "hash-object"
@@ -31,10 +34,8 @@ when "hash-object"
   File.write("./.git/objects/#{sha1[...2]}/#{sha1[2..]}", compress_content)
   print sha1
 when "ls-tree"
-  sha1 = ARGV[-1]
-  file_path = "./.git/objects/#{sha1[...2]}/#{sha1[2..]}"
-  file_raw_source = File.read(file_path)
-  file_source = Zlib.inflate(file_raw_source).split("\x00")[1..]
+  raw_file = read_object(ARGV[-1])
+  file_source = raw_file.split("\x00")[1..]
   file_names_tree = file_source.map { |f| f.split[-1] }.join("\n")
   puts file_names_tree
 else
